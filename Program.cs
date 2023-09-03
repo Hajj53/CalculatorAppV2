493a123ef2e11d64ddcee9f3e6a7275141eed896
Hajj53 <55025756+Hajj53@users.noreply.github.com>
2023-09-03 09:43:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Calculator App\n");
+
+/*How to turn a string (string "45.80") into a number
+
+int num = Convert.ToInt32("45");
+Console.WriteLine(num + 6);
+*/
+
+Console.Write("Enter a number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter another number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num1 + num2);
+
